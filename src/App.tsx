b64e58fda39f87b6926c27bcce89a4ee13b1ae50
rizzytwizzy <127325722+rizzytwizzy@users.noreply.github.com>
2023-12-13 02:42:57 -0500

    Added tokentransfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/gsn-green-vector.svg'
+import viteLogo from './assets/gsn-green-vector.svg'
 import './App.css'
 
 import { BrowserProvider, Contract } from 'ethers'
@@ -15,7 +15,7 @@ const targetFunctionAbiEntry = {
 }
 
 const acceptEverythingPaymasterGoerli = '0x7e4123407707516bD7a3aFa4E3ebCeacfcbBb107'
-const sampleErc2771RecipientAddress = '0xD1cfA489F7eABf322C5EE1B3779ca6Be9Ce08a8e'
+const Erc2771RecipientAddress = '0xF4B948b1aE5635ce9020515e3B77A8634CC87C88'
 
 async function connect() {
   const injected = (window as any).ethereum
@@ -44,7 +44,7 @@ function App() {
     }).then(
       ({gsnSigner}) => {
         console.log('RelayProvider init success')
-        contract.current = new Contract(sampleErc2771RecipientAddress, [targetFunctionAbiEntry], gsnSigner)
+        contract.current = new Contract(Erc2771RecipientAddress, [targetFunctionAbiEntry], gsnSigner)
         setReady(true)
       })
   }, [])
@@ -56,26 +56,45 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>GSN + Vite + React</h1>
+      <h1>Transfer Tokens with GSN (on Goerli)</h1>
       <div className="card">
           {
-              ready ? <button onClick={
-                  () => {
-                      contract.current?.captureTheFlag()
-                  }
-              }> captureTheFlag()
-              </button> : <div> Initializing GSN Provider</div>
+              ready ? (
+                <div>
+                  <input type="text" placeholder="Recipient Address" id="recipientAddress" />
+                  <input type="number" placeholder="Token Amount" id="tokenAmount" />
+                  <button
+
+                    onClick={async () => {
+                      const recipientAddress = document.getElementById("recipientAddress").value;
+                      const tokenAmount = parseInt(document.getElementById("tokenAmount").value);
+
+                      if (!recipientAddress) {
+                        alert("Please enter a valid recipient address.");
+                        return;
+                      }
+            
+                      if (!tokenAmount) {
+                        alert("Please enter a valid token amount.");
+                        return;
+                      }
+
+                      await contract.current?.captureTheFlag()
+                      ;
+                    }}
+                  >
+                    Transfer Tokens
+                  </button>
+                </div>
+              ) : <div> Initializing GSN Provider</div>
           }
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Proof of concept frontend for GSN allowing a user to transfer USDC and pay gas with ERC20 token.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
       <p className="read-the-docs">
         Open Developer Tools for logs, connect MetaMask account and select Goerli network to make a GSN transaction.
       </p>
